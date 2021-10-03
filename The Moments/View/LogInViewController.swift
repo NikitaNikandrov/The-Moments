a0898e322f48886c43c3013183409fcc541aedc3
NikitaNikandrov <nnikandroff@yandex.ru>
2021-10-04 01:03:16 +0300

    added logo icon on login scene

--- The Moments/View/LogInViewController.swift
+++ The Moments/View/LogInViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LogInViewController: UIViewController {
 
+    @IBOutlet weak var iconUIImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
-
