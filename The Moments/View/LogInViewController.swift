ea51f8677b43d9b536bcebf3ed1b4b9c53d0ccb7
NikitaNikandrov <nnikandroff@yandex.ru>
2021-12-17 23:56:41 +0300

    fixed logo & buttons

--- The Moments/View/LogInViewController.swift
+++ The Moments/View/LogInViewController.swift
@@ -8,20 +8,38 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-//Mark: Autlets textfields
+
+    @IBOutlet weak var logoImageView: UIImageView!
+    //Mark: Autlets textfields
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+
 //Mark: Autlets buttons
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var signUpButton: UIButton!
+
 //Mark: Button's actions
     @IBAction func logInButtonIsPressed(_ sender: Any) {
     }
+
 //Mark: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUpButtons()
     }
 
 
+}
+
+extension LogInViewController {
+    
+    func setUpButtons(){
+        self.logInButton.layer.borderWidth = 2
+        self.logInButton.layer.borderColor = UIColor.white.cgColor
+        self.logInButton.layer.cornerRadius = 19
+        
+        self.signUpButton.layer.borderWidth = 2
+        self.signUpButton.layer.cornerRadius = 19
+        self.signUpButton.layer.borderColor = UIColor.white.cgColor
+    }
 }
