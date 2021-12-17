4b376c2b29cba694ac0e6293a203dfa7ad562243
NikitaNikandrov <nnikandroff@yandex.ru>
2021-12-17 23:08:34 +0300

    textfields and buttons on log in screen

--- The Moments/View/LogInViewController.swift
+++ The Moments/View/LogInViewController.swift
@@ -8,9 +8,16 @@
 import UIKit
 
 class LogInViewController: UIViewController {
-
+//Mark: Autlets textfields
     @IBOutlet weak var loginTextField: UITextField!
-    
+    @IBOutlet weak var passwordTextField: UITextField!
+//Mark: Autlets buttons
+    @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+//Mark: Button's actions
+    @IBAction func logInButtonIsPressed(_ sender: Any) {
+    }
+//Mark: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
