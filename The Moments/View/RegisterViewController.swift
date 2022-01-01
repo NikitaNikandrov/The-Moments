df890f506384cb2e9d77a02ff74571ac54d6051d
NikitaNikandrov <nnikandroff@yandex.ru>
2022-01-01 22:42:19 +0300

    created registerVC

--- The Moments/View/RegisterViewController.swift
+++ The Moments/View/RegisterViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+//Mark: Outlets
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+
+//Mark: Actions
+    @IBAction func signUpButtonIsPressed(_ sender: Any) {
+    }
+    
 //Mark: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
