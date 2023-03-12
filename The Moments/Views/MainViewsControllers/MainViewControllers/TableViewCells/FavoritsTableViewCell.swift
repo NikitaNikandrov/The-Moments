9b24f57a738460495659f59c54729cc917a19207
NikitaNikandrov <nnikandroff@yandex.ru>
2023-03-12 20:34:59 +0300

    favourites tableview cell

---
+++ The Moments/Views/MainViewsControllers/MainViewControllers/TableViewCells/FavoritsTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritsTableViewCell.swift
+//  The Moments
+//
+//  Created by Никита on 12.03.2023.
+//
+
+import UIKit
+
+class FavoritsTableViewCell: UITableViewCell {
+    static let cellId = "FavoritsTableViewCell"
+    
+    var cellImageURL: String?
+    var cellTitle: String?
+    var cellDescription: String?
+    
+    
+}
